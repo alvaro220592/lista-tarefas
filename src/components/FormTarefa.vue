709be0fda701cc3d501b992b03bdcbb0d380a9fa
alvaro220592 <alvaro220592@gmail.com>
2026-08-28 21:50:05 -0300

    nova tarefa

--- src/components/FormTarefa.vue
+++ src/components/FormTarefa.vue
@@ -30,7 +30,7 @@ const salvar = () => {
             <v-toolbar-title>{{ props.tituloDialog }}</v-toolbar-title>
 
             <v-toolbar-items>
-                <v-btn text="Salvar" variant="text" @click="salvar"></v-btn>
+                <v-btn text="Salvar" variant="tonal" @click="salvar"></v-btn>
             </v-toolbar-items>
         </v-toolbar>
 
@@ -38,12 +38,11 @@ const salvar = () => {
             <v-card-title>{{ props.tituloDialog }}</v-card-title>
 
             <v-card-text>
-                <v-text-field v-model="titulo" label="Título" variant="outlined" />
+                <v-text-field v-model="titulo" label="Título" variant="outlined" density="compact" />
 
-                <v-textarea v-model="descricao" label="Descrição" variant="outlined" />
+                <v-textarea v-model="descricao" label="Descrição" variant="outlined" density="compact" />
 
-                <v-select v-model="prioridade" label="Prioridade" variant="outlined"
-                    :items="['Baixa', 'Média', 'Alta']" />
+                <v-select v-model="prioridade" label="Prioridade" variant="outlined" density="compact" :items="['Baixa', 'Média', 'Alta']" />
             </v-card-text>
         </v-card>
     </v-dialog>
